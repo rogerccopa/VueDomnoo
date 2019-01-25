9e30f81935765a3ae369283466f6d6888b9693c6
Roger Ccopa <rogerccopa@gmail.com>
2019-01-25 15:50:06 -0500

    Header and Footer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <template>
-<div>
-  Hellow
-</div>
+  <div>
+    <comp-header/>
+    <div>content</div>
+    <comp-footer/>
+  </div>
 </template>
 
 <script>
+import compHeader from "./components/Header-footer/Header.vue";
+import compFooter from "./components/Header-footer/Footer.vue";
 
+export default {
+  components: {
+    compHeader,
+    compFooter
+  }
+};
 </script>
+
+<style>
+@import "./assets/style.css";
+</style>
---
+++ src/components/Header-footer/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <div class="container clear">
+      <div class="logo">
+        <img :src="logoIcon">
+      </div>
+      <div class="nav">
+        <img :src="menuIcon">
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+// Logo will be the url/path to the image (and same the Menu)
+import Logo from "../../assets/images/logo.png";
+import Menu from "../../assets/images/menu.png";
+
+export default {
+  data() {
+    return {
+      logoIcon: Logo,
+      menuIcon: Menu
+    };
+  }
+};
+</script>
