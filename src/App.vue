e081b578b4403d34765c6cb9d2269446147092f3
Roger Ccopa <rogerccopa@gmail.com>
2019-01-28 14:50:14 -0500

    Menu Buttons

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <comp-header/>
     <div>
       <comp-featured/>
+      <comp-menu />
     </div>
     <comp-footer/>
   </div>
@@ -12,12 +13,14 @@
 import compHeader from "./components/Header-footer/Header.vue";
 import compFooter from "./components/Header-footer/Footer.vue";
 import compFeatured from "./components/Featured/index.vue";
+import compMenu from "./components/Menu/index.vue";
 
 export default {
   components: {
     compHeader,
     compFooter,
-    compFeatured
+    compFeatured,
+    compMenu
   }
 };
 </script>
---
+++ src/components/Menu/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container menu_container">
+    <div class="top">
+      <h2>Menu</h2>
+    </div>
+    <div class="buttons">
+      <md-button
+        v-for="(button,index) in buttons"
+        :key="index"
+        :class="button.active?'md-raised':''"
+        @click="changeMenu(button.name)"
+      >{{button.name}}</md-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      buttons: [
+        { name: "pizza", active: true },
+        { name: "burgers", active: false },
+        { name: "salat", active: false },
+        { name: "dessert", active: false }
+      ]
+    };
+  },
+  methods: {
+    changeMenu(btnName) {
+      this.buttons.forEach(button => {
+        button.active = button.name == btnName;
+      });
+    }
+  }
+};
+</script>
