e54ce7ad16ea91bd8430021b640d32fd16d7355d
Roger Ccopa <rogerccopa@gmail.com>
2019-02-05 22:05:26 -0500

    Finishing the Animation

--- src/components/CallUs/anim.vue
+++ src/components/CallUs/anim.vue
@@ -3,25 +3,24 @@
     <transition v-on:before-enter="beforeEnter" v-on:enter="enter" appear>
       <div class="icon icon_phone" delay="0" left="100"></div>
     </transition>
+
+    <transition v-on:before-enter="beforeEnter" v-on:enter="enter" appear>
+      <div class="plus one" delay="100" left="310">+</div>
+    </transition>
+
+    <transition v-on:before-enter="beforeEnter" v-on:enter="enter" appear>
+      <div class="icon icon_delivery" delay="200" left="480"></div>
+    </transition>
+
+    <transition v-on:before-enter="beforeEnter" v-on:enter="enter" appear>
+      <div class="plus" delay="300" left="750">=</div>
+    </transition>
+
+    <transition v-on:before-enter="beforeEnter" v-on:enter="enter" appear>
+      <div class="icon icon_pizza" delay="400" left="900"></div>
+    </transition>
   </div>
 </template>
-
-<style>
-.anim_container {
-  position: relative;
-  height: 200px;
-}
-.icon {
-  background-size: cover !important;
-  position: absolute;
-}
-.icon_phone {
-  background: url("../../assets/images/icons/icon-phone.png");
-  width: 130px;
-  height: 130px;
-  top: 30px;
-}
-</style>
 
 <script>
 export default {
@@ -44,3 +43,36 @@ export default {
   }
 };
 </script>
+
+<style>
+.anim_container {
+  position: relative;
+  height: 200px;
+}
+.icon {
+  background-size: cover !important;
+  position: absolute;
+}
+.icon_phone {
+  background: url("../../assets/images/icons/icon-phone.png");
+  width: 130px;
+  height: 130px;
+  top: 30px;
+}
+.plus {
+  position: absolute;
+  font-size: 160px;
+  color: #ffffff;
+  top: 90px;
+}
+.icon_delivery {
+  background: url("../../assets/images/icons/icon-delivery.png");
+  width: 200px;
+  height: 200px;
+}
+.icon_pizza {
+  background: url("../../assets/images/icons/icon-pizza.png");
+  width: 200px;
+  height: 200px;
+}
+</style>
--- src/components/CallUs/index.vue
+++ src/components/CallUs/index.vue
@@ -4,8 +4,15 @@
       <div class="first" v-scroll-reveal.reset="{delay:200,scarle:2}">
         <h1>Hungry ?</h1>
       </div>
-      <div class="second">
-        <comp-anim/>
+      <div
+        class="second"
+        v-scroll-reveal.reset="{delay:300,afterReveal:()=>{
+          secondAnim=true
+        }}"
+      >
+        <div v-if="secondAnim">
+          <comp-anim/>
+        </div>
       </div>
       <div class="third" v-scroll-reveal.reset="{delay:600,scarle:2}">
         <span>Give us a call</span>
@@ -19,6 +26,11 @@
 import compAnim from "./anim.vue";
 
 export default {
+  data() {
+    return {
+      secondAnim: false
+    };
+  },
   components: {
     compAnim
   }
