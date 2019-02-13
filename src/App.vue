7f36f78738af088739574bd6b2fa170521e099ff
Roger Ccopa <rogerccopa@gmail.com>
2019-02-13 13:02:22 -0500

    Creating the newsletter

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
       <comp-featured/>
       <comp-menu/>
       <comp-call-us/>
+      <comp-newsletter/>
     </div>
     <comp-footer/>
   </div>
@@ -16,6 +17,7 @@ import compFooter from "./components/Header-footer/Footer.vue";
 import compFeatured from "./components/Featured/index.vue";
 import compMenu from "./components/Menu/index.vue";
 import compCallUs from "./components/CallUs/index.vue";
+import compNewsletter from "./components/Newsletter/index.vue";
 
 export default {
   components: {
@@ -23,7 +25,8 @@ export default {
     compFooter,
     compFeatured,
     compMenu,
-    compCallUs
+    compCallUs,
+    compNewsletter
   }
 };
 </script>
---
+++ src/components/Newsletter/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container newsl_container">
+    <h2>Join to our newsletter</h2>
+    <div class="form">
+      <input type="text" v-model="email">
+      <button @click="submitHandler">Subscribe</button>
+    </div>
+    <div class="error_label">
+      <div>{{error}}</div>
+    </div>
+    <div class="success_label">
+      <div>{{success}}</div>
+    </div>
+    <div
+      class="small"
+    >This is some test text for a disclaimer. Read the fine letters very carefully etc. This is some test text for a disclaimer. Read the fine letters very carefully etc.</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      error: "",
+      success: ""
+    };
+  },
+  methods: {
+    validate(email) {
+      let valid = [true, ""];
+
+      const validEmailRegex = /\S+@\S+\.\S+/;
+      if (email === "") {
+        valid = [false, "It is empty"];
+      } else if (!validEmailRegex.test(email)) {
+        valid = [false, "Enter a valid email"];
+      }
+
+      return valid;
+    },
+    submitHandler() {
+      let valid = this.validate(this.email);
+
+      if (valid[0]) {
+        this.error = "";
+        this.addEmail(this.email);
+      } else {
+        this.error = valid[1];
+      }
+    },
+    addEmail(email) {
+      // to implemented in the next video.
+    }
+  }
+};
+</script>
